3d7272787f5f8c55066f9a65b8cd1b3614821789
Abi satya <abiambis19@gmail.com>
2025-03-03 19:09:21 +0700

    ubah logo statis

--- resources/views/dashboard/layouts/navbar.blade.php
+++ resources/views/dashboard/layouts/navbar.blade.php
@@ -1,6 +1,6 @@
 <header class="navbar sticky-top flex-md-nowrap p-0 shadow" style="background-color: #ad1717;" data-bs-theme="dark">
     <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6 text-white" href="/dashboard">
-        <img src="https://semarangkota.go.id/assets/img/logo-icon.png" alt="Logo" 
+        <img src="{{ asset('backend/img/logo-siinfo.png') }}" alt="Logo" 
         style="width: 25px; height: auto;" class="d-inline-block align-text-top me-2">
         SI-INFO</a>
         <ul class="navbar-nav flex-row align-items-center">
